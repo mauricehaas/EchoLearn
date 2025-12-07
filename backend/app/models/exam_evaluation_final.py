3ac5c12233aff68be595ff03a0968f6742f6b140
Aleks1906 <99407925+Aleks1906@users.noreply.github.com>
2025-12-07 19:24:21 +0100

    black formatting pt2

--- backend/app/models/exam_evaluation_final.py
+++ backend/app/models/exam_evaluation_final.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, Text
 from app.core.db import Base
 
 
-
 class ExamEvaluationFinal(Base):
     __tablename__ = "exam_evaluation_final"
 
@@ -10,5 +9,3 @@ class ExamEvaluationFinal(Base):
     unique_exam_id = Column(Text, nullable=False)
     overall_feedback = Column(Text, nullable=False)
     overall_rating = Column(Text, nullable=False)
-
-  
